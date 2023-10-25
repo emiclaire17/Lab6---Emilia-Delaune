d15db5a3189d44d64df51b5f641f16d2e007e8b6
emiclaire17 <emiliadelaune@gmail.com>
2023-10-25 10:11:46 -0400

    Finish the option menu

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+def encode(password):
+    new_password = ""
+    for char in password:
+        if int(char) < 7:
+            char = int(char) + 3
+            new_password = new_password + str(char)
+        elif int(char) == 7:
+            char = 0
+            new_password = new_password + str(char)
+        elif int(char) == 8:
+            char = 1
+            new_password = new_password + str(char)
+        else:
+            char = 2
+            new_password = new_password + str(char)
+    return new_password
+def decode(password):
+def main():
+    active = True
+    while active == True:
+        print('Menu')
+        print('-------------')
+        print('1. Encode\n2. Decode\n3. Quit')
+        print()
+        choice = int(input("Please enter an option: "))
+        if choice == 1:
+            password = input("Please enter your password to encode: ")
+            result = encode(password)
+            print("Your password has been encoded and stored!")
+        if choice == 2:
+            original = decode(result)
+            print(f"The encoded password is {result}, and the original password is {original}")
+        if choice == 3:
+            active = False
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__== '__main__':
+    main()
