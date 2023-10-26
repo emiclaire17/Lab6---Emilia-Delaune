d1b4d1b9fdaa88273d9c7faa57eb95f1c9d2ca0d
emiclaire17 <emiliadelaune@gmail.com>
2023-10-26 13:21:11 -0400

    added comments

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+# Emilia Delaune
 def encode(password):
+    # this function adds three to each number in the original password
     new_password = ""
     for char in password:
         if int(char) < 7:
             char = int(char) + 3
             new_password = new_password + str(char)
         elif int(char) == 7:
+            # when a number hits a digit greater than 7, the count starts back at 0 to ensure no double digits
             char = 0
             new_password = new_password + str(char)
         elif int(char) == 8:
@@ -15,8 +18,10 @@ def encode(password):
             new_password = new_password + str(char)
     return new_password
 def decode(password):
+    # this function decodes the previous encoded password by subtracting 3 from each digit
 def main():
     active = True
+    # allows the menu to print over and over until the user quits
     while active == True:
         print('Menu')
         print('-------------')
